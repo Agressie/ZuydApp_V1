d5025bf9ac3925c24a564b15f6f066a269240bca
Noah <Noah@LAPTOPPIE>
2024-01-25 11:59:08 +0100

    Added some MVVM file structure

---
+++ ZuydApp_V1/MVVM/ViewModels/VM_User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZuydApp_V1.MVVM.ViewModels
+{
+    internal class VM_User
+    {
+    }
+}
