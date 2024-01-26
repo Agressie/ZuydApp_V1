c17f3a1e5adbbd537a758ac4fe9578a8d364fd74
Agressie <noah@lenkens.nl>
2024-01-26 12:05:37 +0100

    Added VM_User Parameters Gives error, Next commit will fix these

--- ZuydApp_V1/MVVM/ViewModels/VM_User.cs
+++ ZuydApp_V1/MVVM/ViewModels/VM_User.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZuydApp_V1.MVVM.Models;
 
 namespace ZuydApp_V1.MVVM.ViewModels
 {
-    internal class VM_User
+    [AddINotifyPropertyChangedInterface]
+    public class VM_User
     {
+        public User? Currentuser { get; set; }
+        public List<User>? Users = new List<User>();
     }
 }
