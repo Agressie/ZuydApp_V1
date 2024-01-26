03862ed0b883f5cd9c6db73ce251d57e6edf918c
Agressie <noah@lenkens.nl>
2024-01-26 12:21:17 +0100

    Fixed Issue with VM_User

--- ZuydApp_V1/MVVM/ViewModels/VM_User.cs
+++ ZuydApp_V1/MVVM/ViewModels/VM_User.cs
@@ -13,55 +13,50 @@ namespace ZuydApp_V1.MVVM.ViewModels
     {
         public static User? Currentuser { get; set; }
         public static List<User>? Users = new List<User>();
-    }
-    public static void CreateNewUser(string username, string password)
-    {
-        Refresh();
-        User user = new User();
-        user.Name = username;
-        user.Password = password;
-        App.UserRepo.SaveEntity(user);
-        Console.WriteLine(App.UserRepo.statusMessage);
 
-    }
-
-    private static void Refresh()
-    {
-        users = App.UserRepo.GetEntities();
-    }
-
-    public static bool LoginCheck(string username, string password)
-    {
-        Refresh();
-        bool result = false;
-        foreach (var user in users)
+        public static void CreateNewUser(string username, string password)
         {
-            if (user.Name == username)
-                if (user.Password == password)
-                {
+            Refresh();
+            User user = new User();
+            user.Name = username;
+            user.Password = password;
+            App.UserRepo.SaveEntity(user);
+            Console.WriteLine(App.UserRepo.statusMessage);
+        }
+
+        private static void Refresh()
+        {
+            Users = App.UserRepo.GetEntities();
+        }
+
+        public static bool LoginCheck(string username, string password)
+        {
+            Refresh();
+            bool result = false;
+            foreach (var user in Users)
+            {
+                if (user.Name == username)
+                    if (user.Password == password)
+                    {
+                        result = true;
+                        Currentuser = user;
+                    }
+            }
+            return result;
+        }
+
+        public static bool Checkusername(string Username)
+        {
+            Refresh();
+            bool result = true;
+            foreach (var user in Users)
+            {
+                if (user.Name == Username)
                     result = true;
-                    currentuser = user;
-                }
+                else
+                    result = false;
+            }
+            return result;
         }
-        return result;
-    }
-
-    public static bool Checkusername(string Username)
-    {
-        Refresh();
-        bool result = true;
-        foreach (var user in users)
-        {
-            if (user.Name == Username)
-                result = true;
-            else
-                result = false;
-        }
-        return result;
-    }
-
-    public void setcurrentuser(User user)
-    {
-        currentuser = user;
     }
 }
