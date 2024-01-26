8f4c9e0612f59af5ab077a8753451b29a0b926d3
Agressie <noah@lenkens.nl>
2024-01-26 11:06:35 +0100

    CreateAccountpage Logic The logic is added only some functions still need te be outcommented when the VM_User is created, After that this code should work fine.

--- ZuydApp_V1/MVVM/Views/CreateAccountPage.xaml.cs
+++ ZuydApp_V1/MVVM/Views/CreateAccountPage.xaml.cs
@@ -1,3 +1,5 @@
+using HomeKit;
+
 namespace ZuydApp_V1.MVVM.Views;
 
 public partial class CreateAccountPage : ContentPage
@@ -6,4 +8,38 @@ public partial class CreateAccountPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    public void Onaccountcreateclicked(object sender, EventArgs e)
+    {
+        var UsernameEmpty = string.IsNullOrEmpty(EntryUsername.Text);
+        var PasswordEmpty = string.IsNullOrEmpty(EntryPassword.Text);
+        var ConfirmPasswordEmpty = string.IsNullOrEmpty(EntryConfirmPassword.Text);
+
+        if (UsernameEmpty)
+            EntryUsername.Placeholder = "Vul iets in!";
+        else if (PasswordEmpty)
+            EntryPassword.Placeholder = "Vul iets in!";
+        else if (ConfirmPasswordEmpty)
+            EntryConfirmPassword.Placeholder = "Vul iets in!";
+        else
+        {   // This Code should work fine only the VM_User is not created yet.
+            //if (VM_User.Checkusername(EntryUsername.Text) == false)
+            //{
+            //    if (EntryPassword.Text == EntryConfirmPassword.Text)
+            //    {
+            //        VM_User.CreateNewUser(EntryUsername.Text, EntryPassword.Text);
+            //        if (VM_User.LoginCheck(EntryUsername.Text, EntryPassword.Text) == true)
+            //        {
+            //            // This function still needs to be added.
+            //            Accountnotification(EntryUsername.Text);
+            //            // THis Page still needs to be added
+            //            Navigation.PushAsync(new HomePage());
+            //        }
+            //    }
+            //    else
+            //        Invalid.Text = "Passwords don't match";
+            //}
+            //else
+            //    Invalid.Text = "Username already exists";
+        }
+    }
 }
