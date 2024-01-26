6bbd9ede89e0501c8c0fcf7d420f788eb377f275
Agressie <noah@lenkens.nl>
2024-01-26 11:02:53 +0100

    Created Login Page Logic There is some code in here that needs to be out commented when the VM_User is created, after that this should work fine.

--- ZuydApp_V1/MVVM/Views/LoginPage.xaml.cs
+++ ZuydApp_V1/MVVM/Views/LoginPage.xaml.cs
@@ -1,3 +1,4 @@
+
 namespace ZuydApp_V1.MVVM.Views;
 
 public partial class LoginPage : ContentPage
@@ -6,4 +7,28 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void Onloginclicked(object sender, EventArgs e)
+    {
+        var UsernameEmpty = string.IsNullOrEmpty(EntryUsername.Text);
+        var PasswordEmpty = string.IsNullOrEmpty(EntryPassword.Text);
+
+        if (UsernameEmpty)
+            EntryUsername.Placeholder = "Vul iets in!";
+        else if (PasswordEmpty)
+            EntryPassword.Placeholder = "Vul iets in!";
+        else
+        {
+            // This code will work when the VM_User is created and this data can be gotten.
+            //if (VM_User.LoginCheck(EntryUsername.Text, EntryPassword.Text) == true)
+            //    // This Page still has to be made
+            //    Navigation.PushAsync(new HomePage());
+            //else
+            //    Invalid.IsVisible = true;
+            //
+        }
+    }
+    private void OnCreateaccountClicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new CreateAccountPage());
+    }
 }
