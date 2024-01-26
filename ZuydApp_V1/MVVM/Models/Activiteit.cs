2405e444f1d8e56d184d055b694d197786f446a7
Agressie <noah@lenkens.nl>
2024-01-26 12:09:53 +0100

    Added Lokaal to Activity

--- ZuydApp_V1/MVVM/Models/Activiteit.cs
+++ ZuydApp_V1/MVVM/Models/Activiteit.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace ZuydApp_V1.MVVM.Models
 {
     internal class Activiteit
     {
+        [Column("lokaal")]
+        public Lokaal lokaal {  get; set; }
     }
 }
