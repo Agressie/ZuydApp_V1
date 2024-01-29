33089b4b9a1766f170247cbd5bfa6ca9129f08cd
Agressie <noah@lenkens.nl>
2024-01-29 16:51:18 +0100

    Added some basic Logic for Event VM

--- ZuydApp_V1/MVVM/ViewModels/VM_Evenement.cs
+++ ZuydApp_V1/MVVM/ViewModels/VM_Evenement.cs
@@ -12,11 +12,12 @@ namespace ZuydApp_V1.MVVM.ViewModels
         public static Evenement? Currentevenement { get; set; }
         public static List<Evenement>? Evenementen = new List<Evenement>();
 
-        public static void CreateNewEvenement(string name)
+        public static void CreateNewEvenement(string name, string description)
         {
             Refresh();
             Evenement evenement = new Evenement();
             evenement.Name = name;
+            evenement.description = description;
             App.EvenementRepo.SaveEntity(evenement);
             Console.WriteLine(App.EvenementRepo.statusMessage);
         }
@@ -29,6 +30,36 @@ namespace ZuydApp_V1.MVVM.ViewModels
         public void ToggleEvenpublicity()
         {
             Currentevenement.eventpublic = !Currentevenement.eventpublic;
+            Savechanges();
+        }
+        
+        public void EditEvenement(string name = null, string description = null, DateTime? dateTime = null)
+        {
+            if (name != null)
+                Currentevenement.Name = name;
+            if (description != null)
+                Currentevenement.description = description;
+            if (dateTime != null)
+                Currentevenement.dateTime = (DateTime)dateTime;
+            Savechanges();
+        }
+        public void AddActivity(Activiteit activiteit)
+        {
+            Currentevenement.activities.Add(activiteit);
+            Savechanges();
+        }
+        public void RemoveActivity(Activiteit activiteit)
+        {
+            Currentevenement.activities.Remove(activiteit);
+        }
+        public void AddUser(User user)
+        {
+            Currentevenement.users.Add(user);
+            Savechanges();
+        }
+        public void RemoveUser(User user)
+        {
+            Currentevenement.users.Remove(user);
             Savechanges();
         }
 
