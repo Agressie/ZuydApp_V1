b24f415802e20f28679b62ee0045dc340314bc07
Noah <Noah@LAPTOPPIE>
2024-01-29 10:58:21 +0100

    Outcommented the functions

--- ZuydApp_V1/MVVM/Views/CreateAccountPage.xaml.cs
+++ ZuydApp_V1/MVVM/Views/CreateAccountPage.xaml.cs
@@ -1,4 +1,5 @@
 using Plugin.LocalNotification;
+using ZuydApp_V1.MVVM.ViewModels;
 
 namespace ZuydApp_V1.MVVM.Views;
 
@@ -21,25 +22,23 @@ public partial class CreateAccountPage : ContentPage
         else if (ConfirmPasswordEmpty)
             EntryConfirmPassword.Placeholder = "Vul iets in!";
         else
-        {   // This Code should work fine only the VM_User is not created yet.
-            //if (VM_User.Checkusername(EntryUsername.Text) == false)
-            //{
-            //    if (EntryPassword.Text == EntryConfirmPassword.Text)
-            //    {
-            //        VM_User.CreateNewUser(EntryUsername.Text, EntryPassword.Text);
-            //        if (VM_User.LoginCheck(EntryUsername.Text, EntryPassword.Text) == true)
-            //        {
-            //            // This function still needs to be added.
-            //            Accountnotification(EntryUsername.Text);
-            //            // THis Page still needs to be added
-            //            Navigation.PushAsync(new HomePage());
-            //        }
-            //    }
-            //    else
-            //        Invalid.Text = "Passwords don't match";
-            //}
-            //else
-            //    Invalid.Text = "Username already exists";
+        {   
+            if (VM_User.Checkusername(EntryUsername.Text) == false)
+            {
+                if (EntryPassword.Text == EntryConfirmPassword.Text)
+                {
+                    VM_User.CreateNewUser(EntryUsername.Text, EntryPassword.Text);
+                    if (VM_User.LoginCheck(EntryUsername.Text, EntryPassword.Text) == true)
+                    {
+                        Accountnotification(EntryUsername.Text);
+                        Navigation.PushAsync(new HomePage());
+                    }
+                }
+                else
+                    Invalid.Text = "Passwords don't match";
+            }
+            else
+                Invalid.Text = "Username already exists";
         }
     }
 
