6af10749a4a3d6f35cf9032707ba63398c9b4bf5
Agressie <noah@lenkens.nl>
2024-01-29 17:23:38 +0100

    Added alle Activiteit VM Functions

--- ZuydApp_V1/MVVM/ViewModels/VM_Activities.cs
+++ ZuydApp_V1/MVVM/ViewModels/VM_Activities.cs
@@ -32,9 +32,50 @@ namespace ZuydApp_V1.MVVM.ViewModels
             Refresh();
         }
 
+        public void EditActiviteit(string name = null, string description = null, DateTime? dateTime = null, int? lokaalid = null)
+        {
+            if (name != null)
+                Currentactiviteit.Name = (string)name;
+            if (description != null)
+                Currentactiviteit.description = (string)description;
+            if (dateTime != null)
+                Currentactiviteit.dateTime = (DateTime)dateTime;
+            if (lokaalid != null)
+                Currentactiviteit.LokaalId = (int)lokaalid;
+            Savechanges();
+        }
+        public void AddUser(User user)
+        {
+            Currentactiviteit.users.Add(user);
+            Savechanges();
+        }
+        // When you want to remove an user to an actviteit call this function.
+        public void RemoveUser(User user)
+        {
+            Currentactiviteit.users.Remove(user);
+            Savechanges();
+        }
+
+        // When you want to get a list with all actviteiten call this function.
+        public List<Activiteit> GetActiviteit()
+        {
+            Refresh();
+            return Activiteiten;
+        }
+        // When you want one specific actviteit call this function and make sure you give the right Event ID.
+        public Activiteit GetSpecificActiviteit(int id)
+        {
+            return App.ActiviteitRepo.GetSpecificEntity(id);
+        }
+
         private static void Refresh()
         {
             Activiteiten = App.ActiviteitRepo.GetEntities();
+        }
+        private static void Savechanges()
+        {
+            App.ActiviteitRepo.SaveEntity(Currentactiviteit);
+            Console.WriteLine(App.ActiviteitRepo.statusMessage);
         }
     }
 }
