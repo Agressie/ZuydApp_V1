6dfe268c889015c62122f5459f1de9a85f0d8983
Agressie <noah@lenkens.nl>
2024-02-01 13:02:14 +0100

    Added First Python import.

---
+++ ZuydApp_V1/API/Dijkstra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Python.Runtime;
+using (Py.GIL());
+
+namespace ZuydApp_V1.API
+{
+    public class Dijkstra
+    {
+        public void getpy()
+        {
+            dynamic pythonModule = Py.Import("Algorithem"); // Vervang dit door de naam van jouw .py-bestand zonder de extensie
+            pythonModule; // Vervang 'jouw_methode' door de naam van de methode die je wilt aanroepen
+
+        }
+    }
+}
+
