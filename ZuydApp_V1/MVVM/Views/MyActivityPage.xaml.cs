ec763f8714572cd294f3f0f56d006344cc233ac3
Agressie <noah@lenkens.nl>
2024-02-03 19:30:33 +0100

    Added navigation to ActivityPages From (My)EventDetailsPage to (My)ActivityPage

---
+++ ZuydApp_V1/MVVM/Views/MyActivityPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ZuydApp_V1.MVVM.Views;
+
+public partial class MyActivityPage : ContentPage
+{
+	public MyActivityPage()
+	{
+		InitializeComponent();
+	}
+}
--- ZuydApp_V1/MVVM/Views/MyEventDetailsPage.xaml.cs
+++ ZuydApp_V1/MVVM/Views/MyEventDetailsPage.xaml.cs
@@ -34,7 +34,7 @@ public partial class MyEventDetailsPage : ContentPage
 
     public void OnActiviteitenClicked(object sender, EventArgs e)
     {
-
+        Navigation.PushAsync(new MyActivityPage());
     }
 
     public void OnInUitschrijvenClicked(object sender, EventArgs e)
